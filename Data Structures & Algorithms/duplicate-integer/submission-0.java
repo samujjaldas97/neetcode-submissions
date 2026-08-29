f8cf3096827a52e00655b71110dfa0d4393c2deb
samujjaldas97 <samujjal.das97@gmail.com>
2026-08-29 20:07:39 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        for(int i=0;i<nums.length;i++) {
+            for(int j=0;j<nums.length && j!=i; j++) {
+                if (nums[i]==nums[j])
+                    return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0;i<nums.length;i++) {
+           set.add(nums[i]);
+        }
+        if (nums.length==set.size())
+            return false;
+        return true;
+    }
+}
